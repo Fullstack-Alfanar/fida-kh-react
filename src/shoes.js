1263db4970c36bc32b16d23bfbe51103864ad9ee
Fida Khoury <62944253+FidaKhoury@users.noreply.github.com>
2022-06-27 01:41:28 +0300

    push/first

---
+++ src/shoes.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+function shoes(props){
+    return(
+        <div className="ShoeCard">
+            <div><label>Company: </label><label>{props.name}</label></div>
+            <div><label>Shoe size: </label><label>{props.size}</label></div>
+            <div><label>Price: </label><label>{props.price}</label></div>
+            <div className="image"><img src={props.img}/></div>
+            
+            
+            
+            
+        </div>
+
+    );
+}
+
+export default shoes;
